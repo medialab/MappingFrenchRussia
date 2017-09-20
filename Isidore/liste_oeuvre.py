13749524a54fe588474fee8aa8ad909934352cc3
samaingw <samain.gwenael@laposte.net>
2017-09-20 11:58:52 +0200

    first attempt (sparql)

--- Isidore/liste_oeuvre.py
+++ Isidore/liste_oeuvre.py
@@ -15,19 +15,31 @@ PREFIX dcterms: <http://purl.org/dc/terms/>
 PREFIX dces: <http://purl.org/dc/elements/1.1/>
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 
-select distinct ?o ?titre ?date ?id ?nomauteur ?sujet where {
-"""+'\n<'+source[:-2]+'>'+"""
- dcterms:title ?titre;
+select distinct ?titre ?date ?id ?nomauteur ?sujet where {
+"""+'\n<'+source[:-1]+'>'+""" dcterms:title ?titre;
  dces:date ?date;
  dcterms:identifier ?id;
  dcterms:creator / foaf:name ?nomauteur;
- dcterms:subject / (skos:prefLabel|skos:altLabel) ?sujet;
+ dcterms:subject / (skos:prefLabel|skos:altLabel) ?sujet.
 }
 """
-		print(query)
+		#print(query)
+		data = []
 		sparql.setQuery(query)
 		sparql.setReturnFormat(JSON)
 		results = sparql.query().convert()
+		for headers in results['head']['vars']:
+			#print(headers, end=" ")
+			data.append(set({}))
+		print()
 		for result in results["results"]["bindings"]:
-			print(result)
+			for i, var in enumerate(results['head']['vars']):
+				#print(result[var]['value'], end=" ")
+				data[i].add(result[var]['value'])
+			#print()
+		for i, headers in enumerate(results['head']['vars']):
+			print(headers, ':', end=" ")
+			for j in data[i]:
+				print(j, end=', ')
+			print()
 	f.close()
