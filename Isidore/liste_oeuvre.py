8abbd1db9bd01e0e6d62952205d45484a54f7c2f
samaingw <samain.gwenael@laposte.net>
2017-09-20 15:36:39 +0200

    added some (optional) fields + CSV generation

--- Isidore/liste_oeuvre.py
+++ Isidore/liste_oeuvre.py
@@ -15,12 +15,17 @@ PREFIX dcterms: <http://purl.org/dc/terms/>
 PREFIX dces: <http://purl.org/dc/elements/1.1/>
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 
-select distinct ?titre ?date ?id ?nomauteur ?sujet where {
+select distinct ?titre ?date ?id ?nomauteur ?nomediteur ?sujet ?resume where {
 """+'\n<'+source[:-1]+'>'+""" dcterms:title ?titre;
  dces:date ?date;
  dcterms:identifier ?id;
  dcterms:creator / foaf:name ?nomauteur;
  dcterms:subject / (skos:prefLabel|skos:altLabel) ?sujet.
+ OPTIONAL{
+"""+'\n{<'+source[:-1]+'>'+"dc:publisher / foaf:name ?nomediteur.} UNION"\
++'\n{<'+source[:-1]+'>'+"dc:description ?resume.}"\
++"""
+ }
 }
 """
 		#print(query)
@@ -28,18 +33,30 @@ select distinct ?titre ?date ?id ?nomauteur ?sujet where {
 		sparql.setQuery(query)
 		sparql.setReturnFormat(JSON)
 		results = sparql.query().convert()
+		#print(results)
 		for headers in results['head']['vars']:
 			#print(headers, end=" ")
 			data.append(set({}))
-		print()
+		#print()
 		for result in results["results"]["bindings"]:
 			for i, var in enumerate(results['head']['vars']):
 				#print(result[var]['value'], end=" ")
-				data[i].add(result[var]['value'])
+				if (var in result):#OPTIONAL clauses make this check mandatory
+#					print(var)
+					data[i].add(result[var]['value'])
 			#print()
+		csv = ""
 		for i, headers in enumerate(results['head']['vars']):
-			print(headers, ':', end=" ")
-			for j in data[i]:
-				print(j, end=', ')
-			print()
+			csv += headers + ','
+		csv = csv[0:-1]+'\n'
+		for i in data:
+			csv+='§'
+			for num, j in enumerate(i):
+				if (num):
+					csv += " // "
+				csv += j
+			csv += '§,'
+		csv = csv[0:-1]+'\n'
+		print(csv)
+		
 	f.close()
