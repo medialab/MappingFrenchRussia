f0ddd7ee83f384e547da1c60085a97fb9c9748ca
samaingw <samain.gwenael@laposte.net>
2017-12-18 12:15:00 +0100

    BnF : Subjects elements matrix generation

---
+++ stats/BnF_keyword_elements_matrix.py
@@ -0,0 +1,27 @@
+import csv, sys
+
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        sys.exit('USAGE : '+sys.argv[0]+' [ElementsList] [PublicationsCSV] [MatrixCSV]')
+    with open(sys.argv[1], 'r') as element_list, open(sys.argv[2], 'r') as f, open(sys.argv[3], 'w') as g:
+        global_keywords_list = [line[0].strip() for line in csv.reader(element_list)]
+        #print(keywords_list)
+        reader = csv.reader(f)
+        matrix = csv.writer(g)
+        record_dict = {}
+        for record in reader:
+            local_keywords_list = record[4].split(' // ')
+            unique_triplet = (record[0], record[1], record[3]) # Author, title, year
+            record_dict[unique_triplet] = [0 for i in global_keywords_list]
+#            print(keywords_list)
+            for keyword in local_keywords_list:
+                #kwd_string = keyword
+                for element in keyword.split('--'):
+                    kwd_string = element.strip()
+                    if kwd_string != "":
+                        record_dict[unique_triplet][global_keywords_list.index(kwd_string)] = 1
+
+        matrix.writerow(global_keywords_list)
+        for _, matrix_row in record_dict.items():
+            matrix.writerow(matrix_row)
+            print(_)
