716f1aa3fbc0d877389e28840f8d4d923f11db4d
samaingw <samain.gwenael@laposte.net>
2018-01-03 13:07:34 +0100

    Join previously splitted lines (because sometimes OpenRefine fails)

---
+++ fold_lines.py
@@ -0,0 +1,26 @@
+import csv, sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.exit('USAGE : '+sys.argv[0]+' [srcCSV] [destCSV]')
+    with open(sys.argv[1], 'r') as f, open(sys.argv[2], 'w') as g:
+        folded_record = []
+        reader = csv.reader(f)
+        writer = csv.writer(g)
+        for not_header, record in enumerate(reader):
+            if not not_header:
+                #writer.writerow(record)
+                folded_record = record
+            else:
+                is_same = True
+                i = 1
+                while i < len(record) and is_same:
+                    is_same = i == 102 or record[i] == "" or record[i] is None
+                    i += 1
+                if is_same:
+                    folded_record[0] += (' // ' + record[0]) if record[0] is not None and record[0] != "" else ""
+                else:
+                    writer.writerow(folded_record)
+                    folded_record = record
+        # Last record to be folded isn't written yet
+        writer.writerow(folded_record)
